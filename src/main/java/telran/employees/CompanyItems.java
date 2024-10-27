4f9af6f8e646ec39ecceb9502fd716cf95ab33c5
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2024-10-27 17:48:44 +0200

    refactor submenu

--- src/main/java/telran/employees/CompanyItems.java
+++ src/main/java/telran/employees/CompanyItems.java
@@ -7,66 +7,78 @@ public class CompanyItems {
     private static EmployeeManagementService service;
 
     public static Item[] getItems(EmployeeManagementService service) {
-
         CompanyItems.service = service;
 
         return new Item[] {
-                Item.of("Add Employee", CompanyItems::hireEmployee),
+                Item.of("Hire Employee", CompanyItems::hireEmployee),
                 Item.of("Display Employee", CompanyItems::displayEmployee),
                 Item.of("Remove Employee", CompanyItems::removeEmployee),
-                Item.of("Display Salary by Department", CompanyItems::displaySalaryByDepartment),
+                Item.of("Display Salary by Department",
+                        CompanyItems::displaySalaryByDepartment),
                 Item.of("Display Departments", CompanyItems::displayListDepartments),
-                Item.of("Display Manager with Most Factor", CompanyItems::displayManagerMostFactor),
+                Item.of("Display Manager with Most Factor",
+                        CompanyItems::displayManagerMostFactor),
                 Item.ofExit()
         };
     }
 
     public static void hireEmployee(InputOutput io) {
-        int basicSalary, wage, hours, percent, factor = 0;
-        long sales = 0;
-        String department = "";
+        Item[] addEmployeeItems = getAddEmployeeItems();
+        Menu menu = new Menu("Hire Employee", addEmployeeItems);
+        menu.perform(io);
+    }
+
+    private static Item[] getAddEmployeeItems() {
+        return new Item[] {
+                Item.of("Hire Regular Employee", CompanyItems::HireRegularEmployee),
+                Item.of("Hire Wage Employee", CompanyItems::hireWageEmployee),
+                Item.of("Hire Sales Person", CompanyItems::hireSalesPerson),
+                Item.of("Hire Manager", CompanyItems::hireManager),
+                Item.ofExit()  
+        };
+    }
+
+    public static void HireRegularEmployee(InputOutput io) {
         long id = io.readLong("Enter Employee ID:", "");
-
-        String menuSelection = "Select the employee type: \n 1. Regular Employee \n 2. Wage Employee \n 3. Sales Person \n. 4. Manager";
-        long employeeType = io.readInt(menuSelection, "");
-
-        if (employeeType == 1) {
-
-            basicSalary = io.readInt("Enter Employee Basic Salary:", "");
-            department = io.readString("Enter Department:");
-            Employee employee = new Employee(id, basicSalary, department);
-            service.hireEmployee(employee);
-
-        } else if (employeeType == 2) {
-
-            basicSalary = io.readInt("Enter Employee Basic Salary:", "");
-            department = io.readString("Enter Department:");
-            wage = io.readInt("Enter Wage:", "");
-            hours = io.readInt("Enter Hours", "");
-            WageEmployee wageEmployee = new WageEmployee(id, basicSalary, department, wage, hours);
-            service.hireEmployee(wageEmployee);
-
-        } else if (employeeType == 3) {
-
-            basicSalary = io.readInt("Enter Employee Basic Salary:", "");
-            department = io.readString("Enter Department:");
-            percent = io.readInt("Enter Percent:", "");
-            sales = io.readLong("Enter Sales", "");
-            wage = io.readInt("Enter Wage:", "");
-            hours = io.readInt("Enter Hours", "");
-            SalesPerson salesPerson = new SalesPerson(id, basicSalary, department, wage, hours, percent, sales);
-            service.hireEmployee(salesPerson);
-
-        } else {
-
-            basicSalary = io.readInt("Enter Employee Basic Salary:", "");
-            department = io.readString("Enter Department:");
-            factor = io.readInt("Enter Factor", "");
-            Manager manager = new Manager(id, basicSalary, department, factor);
-            service.hireEmployee(manager);
-
-        }
-        io.writeLine("Employee hired successfully.");
+        String department = io.readString("Enter Department:");
+        int basicSalary = io.readInt("Enter Employee Basic Salary:", "");
+        Employee employee = new Employee(id, basicSalary, department);
+        service.hireEmployee(employee);
+        io.writeLine("Regular Employee hired successfully.");
+    }
+    
+    public static void hireWageEmployee(InputOutput io) {
+        long id = io.readLong("Enter Employee ID:", "");
+        String department = io.readString("Enter Department:");
+        int basicSalary = io.readInt("Enter Employee Basic Salary:", "");
+        int wage = io.readInt("Enter Wage:", "");
+        int hours = io.readInt("Enter Hours Worked:", "");
+        WageEmployee wageEmployee = new WageEmployee(id, basicSalary, department, wage, hours);
+        service.hireEmployee(wageEmployee);
+        io.writeLine("Wage Employee hired successfully.");
+    }
+    
+    public static void hireSalesPerson(InputOutput io) {
+        long id = io.readLong("Enter Employee ID:", "");
+        String department = io.readString("Enter Department:");
+        int basicSalary = io.readInt("Enter Employee Basic Salary:", "");
+        int wage = io.readInt("Enter Wage:", "");
+        int hours = io.readInt("Enter Hours Worked:", "");
+        int percent = io.readInt("Enter Percent:", "");
+        long sales = io.readLong("Enter Sales", "");
+        SalesPerson salesPerson = new SalesPerson(id, basicSalary, department, wage, hours, percent, sales);
+        service.hireEmployee(salesPerson);
+        io.writeLine("Sales Person hired successfully");
+    }
+    
+    public static void hireManager(InputOutput io) {
+        long id = io.readLong("Enter Employee ID:", "");
+        String department = io.readString("Enter Department:");
+        int basicSalary = io.readInt("Enter Employee Basic Salary:", "");
+        int factor = io.readInt("Enter Factor", "");
+        Manager manager = new Manager(id, basicSalary, department, factor);
+        service.hireEmployee(manager);
+        io.writeLine("Manager hired successfully");
     }
 
     public static void displayEmployee(InputOutput io) {
